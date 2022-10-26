aca3f6762609f4bf688cd9f2785fdd877a99c943
Elizabeth Bailey <52120502+dangerbailey@users.noreply.github.com>
2022-10-26 12:17:49 -0700

    Update library.py

--- library.py
+++ library.py
@@ -287,3 +287,20 @@ class KNNTransformer(BaseEstimator, TransformerMixin):
   def fit_transform(self, X, y=None):
     X_ = self.transform(X)
     return X_
+  
+def find_random_state(df, labels, n = 200):
+  for i in range(1, n):
+    train_X, test_X, train_y, test_y = train_test_split(df, labels, test_size=0.2, shuffle=True,
+                                                    random_state=i, stratify=labels)
+    model.fit(train_X, train_y)  #train model
+    train_pred = model.predict(train_X)           #predict against training set
+    test_pred = model.predict(test_X)             #predict against test set
+    train_f1 = f1_score(train_y, train_pred)   #F1 on training predictions
+    test_f1 = f1_score(test_y, test_pred)      #F1 on test predictions
+    f1_ratio = test_f1/train_f1          #take the ratio
+    var.append(f1_ratio)
+
+  rs_value = sum(var)/len(var)  #get average ratio value
+  rs_value 
+  idx = np.array(abs(var - rs_value)).argmin()  #find the index of the smallest value
+  return idx
