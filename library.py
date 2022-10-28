9aea9a97a404e301880516495978a924c3cb3edf
Elizabeth Bailey <52120502+dangerbailey@users.noreply.github.com>
2022-10-27 22:14:37 -0700

    added statements to import KNeighbors and f1_score

--- library.py
+++ library.py
@@ -4,6 +4,8 @@ from joblib.logger import pprint
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer
+from sklearn.metrics import f1_score
+from sklearn.neighbors import KNeighborsClassifier
 
 #This class maps values in a column, numeric or categorical.
 class MappingTransformer(BaseEstimator, TransformerMixin):
